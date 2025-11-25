1f3368ea8f792b9fa933064fee955b5cc9938230
Thomas <andrewearl2324@gmail.com>
2025-11-25 16:42:06 +0000

    Added HTTPS

--- src/main/java/comp3911/cwk2/AppServer.java
+++ src/main/java/comp3911/cwk2/AppServer.java
@@ -1,13 +1,10 @@
 package comp3911.cwk2;
 
-import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.StdErrLog;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
-import org.eclipse.jetty.servlet.FilterHolder;
-import javax.servlet.DispatcherType;
-import java.util.EnumSet;
 
 public class AppServer {
 
@@ -17,7 +14,30 @@ public class AppServer {
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(AppServlet.class, "/*");
 
-        Server server = new Server(8080);
+        Server server = new Server();
+
+        //OLD HTTP CONNECTION
+        //ServerConnector http = new ServerConnector(server);
+        //http.setPort(8080);
+        //server.addConnector(http);
+
+        HttpConfiguration httpsConfig = new HttpConfiguration();
+        httpsConfig.addCustomizer(new SecureRequestCustomizer());
+
+        SslContextFactory.Server ssl = new SslContextFactory.Server();
+
+        ssl.setKeyStorePath("keystore.jks");
+        ssl.setKeyStorePassword("Jqx6weadu7qv");
+        ssl.setKeyManagerPassword("Jqx6weadu7qv");
+
+        ServerConnector https = new ServerConnector(
+                server,
+                new SslConnectionFactory(ssl, "http/1.1"),
+                new HttpConnectionFactory(httpsConfig)
+        );
+        https.setPort(8443);
+        server.addConnector(https);
+
         server.setHandler(handler);
 
         server.start();
